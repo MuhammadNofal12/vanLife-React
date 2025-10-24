d25860d8d9896677cb015b3e53778359a62f2cfd
Muhammad Nofal12 <muhammadnofal446@gmail.com>
2025-10-24 17:48:39 +0500

    Added Vans, Host pages, components, and updated styling

---
+++ src/components/HostLayout.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { NavLink, Link, Outlet } from "react-router-dom";
+
+export default function HostLayout() {
+  const activeStyle = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#c71010ff",
+  };
+  return (
+    <>
+      <nav className="host-nav">
+        <NavLink
+          to="."
+          end
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="income"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Income
+        </NavLink>
+        <NavLink
+          to="vans"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Vans
+        </NavLink>
+        <NavLink
+          to="reviews"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Reviews
+        </NavLink>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
---
+++ src/pages/Host/HostVans.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { data, Link } from "react-router-dom";
+
+export default function HostVans() {
+  const [vans, setVans] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/api/host/vans")
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
+  const hostVansEls = vans.map((van) => (
+    <Link to={van.id} key={van.id} className="host-van-link-wrapper">
+      <div className="host-van-single" key={van.id}>
+        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+        <div className="host-van-info">
+          <h3>{van.name}</h3>
+          <p>${van.price}</p>
+        </div>
+      </div>
+    </Link>
+  ));
+  return (
+    <section>
+      <h1 className="host-vans-title">Your listed vans</h1>
+      <div className="host-vans-list">
+        {vans.length > 0 ? (
+          <section>{hostVansEls}</section>
+        ) : (
+          <h2>Loading...</h2>
+        )}
+      </div>
+    </section>
+  );
+}
